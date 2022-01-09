361cb00c097cb9a903624f9b653775de87a0043e
Gabriel Nakaema <gsnakaema@gmail.com>
2022-01-08 23:06:10 -0300

    feat: turn user admin

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const foundUser = this.usersRepository.findById(user_id);
+
+    if (!foundUser) {
+      throw new Error("User not found");
+    }
+
+    foundUser.admin = true;
+
+    return foundUser;
   }
 }
 
